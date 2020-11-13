feadd02b4bd4d7bf33486eb0085b6872c2811a7b
Tyler Williams <williams.tyler@gmail.com>
2020-11-13 09:29:03 -0800

    Pre-MVP sidecar starter / updater.

--- cmd/buildbuddy/buildbuddy.go
+++ cmd/buildbuddy/buildbuddy.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/bazelbuild/bazelisk/core"
 	"github.com/bazelbuild/bazelisk/repositories"
+	"github.com/buildbuddy-io/buildbuddy-cli/sidecar"
 )
 
+func die(exitCode int, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(exitCode)
+}
+
 func main() {
-	// For now, just do what bazelisk does!
+	ctx := context.Background()
+
+	// Make sure we have a home directory to work in.
+	bbHome := core.GetEnvOrConfig("BUILDBUDDY_HOME")
+	if len(bbHome) == 0 {
+		userCacheDir, err := os.UserCacheDir()
+		if err != nil {
+			die(-1, err)
+		}
+		bbHome = filepath.Join(userCacheDir, "buildbuddy")
+	}
+
+	updated, err := sidecar.MaybeUpdateSidecar(ctx, bbHome)
+	if err != nil {
+		log.Printf("Error updating sidecar: %s", err.Error())
+	}
+	log.Printf("Updated sidecar: %t", updated)
+
+	sidecarSocket, err := sidecar.RestartSidecarIfNecessary(ctx, bbHome)
+	if err == nil {
+		os.Args = append(os.Args, fmt.Sprintf("--bes_backend=unix://%s", sidecarSocket))
+	}
+
+	// Run bazel.
 	gcs := &repositories.GCSRepo{}
 	gitHub := repositories.CreateGitHubRepo(core.GetEnvOrConfig("BAZELISK_GITHUB_TOKEN"))
 	// Fetch releases, release candidates and Bazel-at-commits from GCS, forks from GitHub
 	repos := core.CreateRepositories(gcs, gcs, gitHub, gcs, true)
 
 	exitCode, err := core.RunBazelisk(os.Args[1:], repos)
-	if err != nil {
-		log.Fatal(err)
-	}
-	os.Exit(exitCode)
+	die(exitCode, err)
+
 }
