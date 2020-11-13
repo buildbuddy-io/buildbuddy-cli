0b4b96f6960f3d0b607eb1284de16cca59a1a20c
Tyler Williams <williams.tyler@gmail.com>
2020-11-13 09:17:29 -0800

    Add a download lib to check github for releases.

---
+++ download/download.go
@@ -0,0 +1,67 @@
+package download
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/google/go-github/github"
+)
+
+const (
+	bbOwner = "buildbuddy-io"
+	bbRepo  = "buildbuddy"
+)
+
+type Binary interface {
+	Version() string
+	Download(ctx context.Context, outputPath string) error
+}
+
+type binaryImpl struct {
+	version     string
+	downloadURL string
+}
+
+func (b *binaryImpl) Version() string { return b.version }
+func (b *binaryImpl) Download(ctx context.Context, outputPath string) error {
+	rsp, err := http.Get(b.downloadURL)
+	if err != nil {
+		return err
+	}
+	defer rsp.Body.Close()
+	f, err := os.OpenFile(outputPath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return status.InternalErrorf("Error writing sidecar to file: %s", err.Error())
+	}
+	defer f.Close()
+	_, err = io.Copy(f, rsp.Body)
+	return err
+}
+
+func GetLatestSidecarFromGithub(ctx context.Context, sidecarName string) (Binary, error) {
+	client := github.NewClient(nil)
+	releases, _, err := client.Repositories.ListReleases(ctx, bbOwner, bbRepo, nil)
+	if err != nil {
+		return nil, err
+	}
+	allowPrereleases := os.Getenv("BB_PRERELEASE") != ""
+	for _, release := range releases {
+		if *release.Prerelease && !allowPrereleases {
+			log.Printf("Skipping prerelease %q", *release.TagName)
+			continue
+		}
+		for _, asset := range release.Assets {
+			if *asset.Name == sidecarName {
+				return &binaryImpl{
+					version:     *release.TagName,
+					downloadURL: *asset.BrowserDownloadURL,
+				}, nil
+			}
+		}
+	}
+	return nil, status.NotFoundErrorf("No sidecar found on github matching name %q", sidecarName)
+}
